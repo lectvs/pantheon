962f9f8f2c7325ea9400793b36d4a74c1827e032
Hayden McCraw <lectvs@gmail.com>
2021-06-09 20:58:27 -0700

    Undo cnoise normalization :(

--- lectvs/utils/perlin.ts
+++ lectvs/utils/perlin.ts
@@ -103,11 +103,13 @@ class Perlin {
     ];
 
     // From https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
+    // Normalized to (-1, 1)
     static readonly SHADER_SOURCE = `
         vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
         vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
         vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
 
+        // Normalized to (-1, 1)
         float cnoise(vec3 P){
             vec3 Pi0 = floor(P); // Integer part for indexing
             vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
@@ -173,9 +175,7 @@ class Perlin {
             vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
             vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
             float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
-            float negone_to_one = 2.2 * n_xyz;
-            float zero_to_one = (negone_to_one + 1.0) / 2.0;
-            return zero_to_one;
+            return 2.2 * n_xyz;
         }
     `;
 }
