e00048c4b406ab99f5c7f9125c621f05114e009b
Hayden McCraw <lectvs@gmail.com>
2022-08-09 23:23:47 -0700

    Update PhysicsUtils

--- world/physics/physicsUtils.ts
+++ world/physics/physicsUtils.ts
@@ -7,6 +7,12 @@ class PhysicsUtils {
         if (v.y < 0) v.y = Math.min(v.y + fy * delta, 0);
     }
 
+    static applyFriction1d(v: number, f: number, delta: number) {
+        if (v > 0) return Math.max(v - f * delta, 0);
+        if (v < 0) return Math.min(v + f * delta, 0);
+        return 0;
+    }
+
     static smartAccelerate(v: Vector2, ax: number, ay: number, delta: number, maxSpeed: number) {
         let oldMagnitude = v.magnitude;
 
@@ -19,8 +25,12 @@ class PhysicsUtils {
     }
 
     static smartAccelerate1d(v: number, a: number, delta: number, maxSpeed: number) {
-        let vv = vec2(v, 0);
-        this.smartAccelerate(vv, a, 0, delta, maxSpeed);
-        return vv.x;
+        v += a * delta;
+
+        if (Math.abs(v) > maxSpeed) {
+            v = Math.sign(v) * maxSpeed;
+        }
+
+        return v;
     }
 }
