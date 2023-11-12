c8a1c0a857f4b00f3c95f6258310fc7603778c46
Hayden McCraw <lectvs@gmail.com>
2023-11-11 16:17:00 -0800

    Load custom resources after assets

--- engine/load/loaderSystem.ts
+++ engine/load/loaderSystem.ts
@@ -6,23 +6,35 @@ class LoaderSystem {
     }
 
     load(progressCallback: (progress: number) => void, callback: () => void) {
-        for (let loader of this.loaders) {
-            loader.load(() => this.onLoaderLoad(progressCallback, callback));
-        }
+        let assetLoaders = this.loaders.filter(loader => !(loader instanceof CustomResourceLoader));
+        let customLoaders = this.loaders.filter(loader => loader instanceof CustomResourceLoader);
+
+        this.loadLoaders(assetLoaders, progressCallback, () => {
+            this.loadLoaders(customLoaders, progressCallback, () => {
+                callback();
+            });
+        });
     }
 
-    private onLoaderLoad(progressCallback: (progress: number) => void, callback: () => void) {
-        progressCallback(this.getLoadProgress());
-        if (this.isLoadComplete()) {
+    private loadLoaders(loaders: Loader[], progressCallback: (progress: number) => void, callback: () => void) {
+        if (loaders.length === 0) {
+            progressCallback(this.getLoadProgress());
             callback();
+            return;
+        }
+
+        for (let loader of loaders) {
+            loader.load(() => {
+                progressCallback(this.getLoadProgress());
+                if (loaders.every(loader => loader.completionPercent >= 1)) {
+                    callback();
+                }
+            });
         }
     }
 
     private getLoadProgress() {
+        if (this.loaders.length === 0) return 1;
         return A.sum(this.loaders, loader => loader.completionPercent) / this.loaders.length;
-    }
-
-    private isLoadComplete() {
-        return this.loaders.every(loader => loader.completionPercent >= 1);
     }
 }
